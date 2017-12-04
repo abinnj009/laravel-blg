ca7f64f5f5bddac755b16664a2d1af4b1c6a20e1
Abin James <abinnj009@gmail.com>
2017-12-04 14:15:45 +0530

    Users creation completed. #205

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,34 @@
-users index
+@extends('layouts.admin')
+
+@section('content')
+
+<h1> Users </h1>
+
+ <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+	<th>Role</th>
+	<th>Status</th>
+	<th>Created at</th>
+	<th>Updated at</th>
+      </tr>
+    </thead>
+    <tbody>
+	@foreach($users as $user)
+      <tr>
+        <td>{{$user->id}}</td>
+        <td>{{$user->name}}</td>
+        <td>{{$user->email}}</td>
+	<td>{{$user->role->name}}</td>
+	<td>{{$user->is_active ? "Active" : "Inactive" }} </td>
+	<td>{{$user->created_at}}</td>
+	<td>{{$user->updated_at->diffForHumans()}}</td>
+      </tr>
+	@endforeach
+    </tbody>
+  </table>
+
+@stop
